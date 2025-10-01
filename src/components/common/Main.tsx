4cbf3a13ddb9f9ba7f386465e8aa35fbd2cb11f3
KIMDJ\vzmzm <azmzm@naver.com>
2025-10-01 15:08:41 +0900

    [dj][2025-10-01]

--- src/components/common/Main.tsx
+++ src/components/common/Main.tsx
@@ -1,12 +1,8 @@
 'use client';
-import React, {useEffect} from 'react';
+import React from 'react';
 import Link from "next/link";
 
 export default function Main() {
-
-    useEffect(() => {
-        // setBrandColors('#00a27a'); // 프로젝트 메인컬러만 교체
-    }, []);
 
     return (
         <div className="p-4">
