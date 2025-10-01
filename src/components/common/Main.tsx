72dceefa0ce22baaa1b768931cf6ae4f6d05322c
KIMDJ\vzmzm <azmzm@naver.com>
2025-10-01 19:18:36 +0900

    [dj][2025-10-01] 메인 수정

--- src/components/common/Main.tsx
+++ src/components/common/Main.tsx
@@ -1,17 +1,26 @@
 'use client';
 import React from 'react';
 import Link from "next/link";
+import {Dot} from "lucide-react";
 
 export default function Main() {
 
     return (
         <div>
-            <ul>
+            <ul className="font-bold underline">
                 <li>
-                    <Link href="/tools/alert" className="underline">Alert</Link>
+                    <Link href="/tools/alert">
+                        <span className="flex justify-start items-center">
+                            <Dot/>Alert
+                        </span>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/tools/list" className="underline">list</Link>
+                    <Link href="/tools/list">
+                        <span className="flex justify-start items-center">
+                            <Dot/>List
+                        </span>
+                    </Link>
                 </li>
             </ul>
         </div>
